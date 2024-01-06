0aa0d31940dfc97ef6d5ede9519ac6f7ab6abfe2
ruturaj1009 <ruturajsahu2002@gmail.com>
2024-01-06 15:36:57 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env : {
+        API_KEY: "AIzaSyD6wh6ykL0kv1aBuxmZc0can0CxPvawSts",
+    }
+}
+
+module.exports = nextConfig
---
+++ src/firebase/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { auth, firestore, app };
